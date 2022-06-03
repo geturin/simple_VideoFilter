ea71a51d5f89bc7a2d0916974a2c53fa718cb3eb
geturin <keroll551@gmail.com>
2022-06-03 21:28:53 +0900

    add:depth_filter

--- src/segmentation.py
+++ src/segmentation.py
@@ -13,7 +13,7 @@ class segmentation_filter(object):
     
     def get_filter_img(self,target_img,bg_img,filter_model):
         self.img=target_img
-        if bg_img.all()!=None:
+        if isinstance(bg_img, np.ndarray):
             self.bg=bg_img
         else:
             self.bg=cv2.GaussianBlur(self.img,(0,0),15)
@@ -25,3 +25,23 @@ class segmentation_filter(object):
             self.img[np.where(self.mask>0.6)]=self.bg[np.where(self.mask>0.6)]
 
         return self.img
+
+class filter_with_mask(object):
+    def __init__(self,threshold) -> None:
+        self.threshold=threshold
+        pass
+    def filter_img(self,target_img,bg_img,mask,filter_model):
+        self.img=target_img
+        self.mask=mask
+        if isinstance(bg_img, np.ndarray):
+            self.bg=bg_img
+        else:
+            self.bg=cv2.GaussianBlur(self.img,(0,0),15)
+
+        if filter_model==0:
+            self.img[np.where(self.mask<self.threshold)]=self.bg[np.where(self.mask<self.threshold)]
+        else:
+            self.img[np.where(self.mask>self.threshold)]=self.bg[np.where(self.mask>self.threshold)]
+
+        return self.img
+        
